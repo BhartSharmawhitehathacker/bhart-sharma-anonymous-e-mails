f7f60014748a11ddf511261e5381082695555cf3
BhartSharmawhitehathacker <59437165+BhartSharmawhitehathacker@users.noreply.github.com>
2020-03-16 01:23:04 +0530

    Add files via upload
    Bsf anonymous email . Is tool in these tool you can select any anonymous emails . For used in untrusted website & apps . Anywhere . Thank you author bhart sharma

---
+++ bsf anonymous mails.sh
@@ -0,0 +1,317 @@
+#!/bin/bash
+
+apt update
+
+apt upgrade 
+
+
+
+
+figlet -f big Bhart sharma anonymous E-mails
+
+
+
+echo " author Bhart sharma
+
+
+---------------
+
+
+"
+
+
+
+echo " instagram varun_sharma4578 
+
+
+
+------------------
+
+
+
+"
+
+
+
+
+
+echo "website www.bhartsharmaframeworkteam.com 
+
+--------------
+
+"
+
+
+
+
+echo " This is your anonymous mails 
+
+Loading ----- /
+
+
+
+
+"
+
+echo "007"
+echo "007"
+echo "007
+
+
+
+"
+
+
+
+
+
+
+
+echo " wakima2576@newe-mail.com 
+
+
+
+"
+
+
+
+
+
+echo " Write your own passwords 
+
+
+
+
+"
+
+
+
+
+
+
+
+echo " wayogix105@hxqmail.com  
+
+
+
+
+"
+
+
+
+
+echo " select your own passwords 
+
+
+
+
+"
+
+
+
+
+
+
+echo " royeg44953@mailezee.com 
+
+
+
+
+"
+
+
+
+
+
+echo " select your own passwords 
+
+
+
+"
+
+
+echo " riconol189@mailezee.com 
+
+
+
+"
+
+
+
+
+echo " select your own passwords 
+
+
+
+
+
+"
+
+
+
+echo " kewexa5175@emailnube.com 
+
+
+
+"
+
+
+
+
+
+echo " select your own passwords 
+
+
+
+
+
+
+
+"
+
+
+
+
+
+
+
+
+echo " tibar99099@newe-mail.com 
+
+
+
+
+
+"
+
+
+
+
+
+
+
+echo " write your own password 
+
+
+
+
+
+
+
+"
+
+
+
+
+
+
+echo " fireli3329@provamail.com 
+
+
+
+
+
+
+"
+
+
+
+
+
+
+
+echo " Write your own password 
+
+
+
+
+
+
+"
+
+
+
+
+
+echo " ninose1580@mailernam.com 
+
+
+
+
+
+
+"
+
+
+
+
+
+echo " make yOur own passwords 
+
+
+
+
+"
+
+
+
+
+
+
+echo " telic85237@mailernam.com 
+
+
+
+
+
+
+"
+
+
+
+
+
+echo " make your own password 
+
+
+
+
+
+
+
+"
+
+
+
+
+echo "completed !
+
+
+
+
+"
+
+
+
+
+echo "this is only verision 1.0 . Wait for verision 1.1 in future for more emails 
+
+
+
+
+
+"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
